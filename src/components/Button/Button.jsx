b342234d950f28253017f88f5695ee47b1a7ceb2
Mingxia Zeng <emiliezeng91@gmail.com>
2024-09-03 19:19:24 -0400

    fix: refine styles

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import './Button.scss';
 
 const Button = ({ children, type, onClick, disabled, status="primary" }) => {
   return (
-    <button className={`button ${status ? `button--${status}` : ''}`} type={type} onClick={onClick} disabled={disabled}>
+    <button className={`button button--${status}`} type={type} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
