6cd82ab1c8585721d560967ad25251a7a8c37746
AbdulKabir Sulaiman <justcabyr@gmail.com>
2024-09-04 15:23:47 -0230

    chore: updated components directory to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import './App.scss';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header';
-import WarehouseList from './components/WarehouseList/WarehouseList';
-import InventoryList from './components/InventoryList/InventoryList';
-import NotFoundPage from './components/NotFoundPage/NotFoundPage';
-import WarehouseCard from './components/WarehouseCard/WarehouseCard';
-import InventoryCard from './components/InventoryCard/InventoryCard';
-import AddWarehouse from './components/AddWarehouse/AddWarehouse';
-import AddInventory from './components/AddInventory/AddInventory';
-import EditWarehouse from './components/EditWarehouse/EditWarehouse';
-import EditInventory from './components/EditInventory/EditInventory';
+import WarehouseList from './pages/WarehouseList/WarehouseList';
+import InventoryList from './pages/InventoryList/InventoryList';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import WarehouseCard from './pages/WarehouseCard/WarehouseCard';
+import InventoryCard from './pages/InventoryCard/InventoryCard';
+import AddWarehouse from './pages/AddWarehouse/AddWarehouse';
+import AddInventory from './pages/AddInventory/AddInventory';
+import EditWarehouse from './pages/EditWarehouse/EditWarehouse';
+import EditInventory from './pages/EditInventory/EditInventory';
 import Footer from './components/Footer/Footer';
 import DeleteWarehouse from './pages/DeleteWarehouse/DeleteWarehouse';
 
