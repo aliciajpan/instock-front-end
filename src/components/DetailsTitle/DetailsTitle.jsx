8bb871c75bbe7e40bd6934aedd3d897e26f041d4
Alicia Pan <alicia.pan@uwaterloo.ca>
2024-09-07 16:59:54 -0400

    Implement inventory details page, adjust spacing on some related components, fix edit button links

--- src/components/DetailsTitle/DetailsTitle.jsx
+++ src/components/DetailsTitle/DetailsTitle.jsx
@@ -1,26 +1,29 @@
+import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button';
 import backArrowIcon from '../../assets/icons/arrow_back-24px.svg';
 import editIcon from '../../assets/icons/edit-white-24px.svg';
 import "./DetailsTitle.scss";
 
 function DetailsTitle({itemName, backLink, editLink}) {
+    const navigate = useNavigate();
+
     return (
-            <article className='whdetails__title'>
-            <div className='whdetails__name'>
-                <img className='whdetails__back-icon' onClick={() => navigate({backLink})} src={backArrowIcon} alt="Back arrow icon" />
+        <article className='details__title'>
+            <div className='details__name'>
+                <img className='details__back-icon' onClick={() => navigate(backLink)} src={backArrowIcon} alt="Back arrow icon" />
                 <h1>{itemName}</h1>
             </div>
 
-            <div className='whdetails__edit-container'>
-                <div className='whdetails__mobile-edit'>
-                    <Button onClick={() => navigate({editLink})}>
-                            <img className='whdetails__edit-icon' src={editIcon} alt="Edit icon" />
+            <div className='details__edit-container'>
+                <div className='details__mobile-edit'>
+                    <Button onClick={() => navigate(editLink)}>
+                            <img className='details__edit-icon' src={editIcon} alt="Edit icon" />
                     </Button>
                 </div>
 
-                <div className='whdetails__tablet-edit'>
-                    <Button onClick={() => navigate({editLink})}>
-                            <img className='whdetails__edit-icon' src={editIcon} alt="Edit icon" /> <span>Edit</span>
+                <div className='details__tablet-edit'>
+                    <Button onClick={() => navigate(editLink)}>
+                            <img className='details__edit-icon' src={editIcon} alt="Edit icon" /> <span>Edit</span>
                     </Button>
                 </div>
             </div>
